d2866d1bc3ed55c66f6ff1453743111974b87637
Aritra Basak <basakaritra10@gmail.com>
2022-06-09 02:40:00 +0530

    Create Throws_Exception.java

---
+++ Throws_Exception.java
@@ -0,0 +1,21 @@
+// Java program to demonstrate working of throws
+class ThrowsExcep
+{
+	static void fun() throws ArithmeticException //the method which causes exception throws the exception
+	{
+		System.out.println("Inside fun() ");
+		int a =10,b =0;
+    int c=a/b;// throw an exception explicitly            
+	}
+	public static void main(String args[])
+	{
+		try
+		{
+			fun();
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("caught in main.");
+		}
+	}
+}
