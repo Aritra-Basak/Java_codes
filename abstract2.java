f5158c8f58f06fd754e6eeb58c0b8a587c191983
Aritra Basak <basakaritra10@gmail.com>
2022-06-15 16:32:23 +0530

    Create abstract2.java

---
+++ abstract2.java
@@ -0,0 +1,33 @@
+abstract class Abs
+{
+Abs(int a)
+{
+System.out.println("Constructor of abstract class "+a);
+}
+abstract void msg1();
+static void msg2()
+{
+System.out.println("Non abstract method of abstract class");
+}
+}
+class A extends Abs
+{
+A()
+{
+super(10);
+System.out.println("Constructor of Class A");
+}
+void msg1()
+{
+System.out.println("overriding abstract method of abstract class");
+}
+}
+class Demo
+{
+public static void main(String args[])
+{
+A a=new A();
+a.msg1();
+Abs.msg2();
+}
+}
