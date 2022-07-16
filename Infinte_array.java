e6b19b78349fa6a0fa41ce24e6e3a33d1bcbd770
Aritra Basak <basakaritra10@gmail.com>
2022-07-16 15:42:38 +0530

    Update Infinte_array.java

--- Infinte_array.java
+++ Infinte_array.java
@@ -1,15 +1,14 @@
-//we will find the target element in a infinite length array.
-//to mimic inifinte array we will not work with the length of the array.Instead we will work with part by part of the array.
+//Here we have to find the target element from an array of infinite array.
+//to mimic infinte array we will not work with the length of the array.Instead we will work with part by part of the array.
 package technicals;
 import java.util.*;
 public class Infinite {
-	static int range(int []ar,int target)//this function will pass the staring and ending index of the array
+	static int range(int []ar,int target)
 	{
-		int start =0,end=1,newstart=0;
-    //we will increase the part of the array by doubling the size of the array each steps till the condition satisfy
+		int start =0,end=1;
 		while(target>ar[end])
 		{
-			newstart =end+1;//just shifting the start index after the previous end index
+			int newstart =end+1;//just shifting the start index after the previous end index
 			end=end+(2*(end-start+1));//end will be the double of the previous part length.
 			start=newstart;//updating the original value of start;
 			
@@ -53,3 +52,21 @@ public class Infinite {
 	}
 
 }
+/*Enter the array size
+10
+Enter the elements in the array in a sorted manner
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Enter the target element
+5
+Element is present in the index :4 of the infinite array
+*/
