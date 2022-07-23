6949776aaf8a0656e2063be637eb35c39091a380
Aritra Basak <basakaritra10@gmail.com>
2022-07-23 13:39:40 +0530

    Create Multi-Threading_Example1.java

---
+++ Multi-Threading_Example1.java
@@ -0,0 +1,60 @@
+/*In the below program we see the use of multi-threading that we are forcing the CPU to use it's multiple cores.
+ * We will make the CPU do each task in each cores.
+ * By default JAVA runs a thread at every time we run a program and that thread is called Main Thread.  */
+package placements;
+class A extends Thread //we are extending this class as thread as to make java run each class in each core i.e. running both the classes parallel.
+{
+	public void run()// we have written the method name as run() 
+	//because to start a thread we need to call start method from Thread class with our classes object references 
+	//and that start can only call the run method()
+	//So we can say to create a thread we need to extends to Thread class, run() and call start() with object references of our class.
+	{
+		for(int i=1;i<=10;i++)
+		{
+		System.out.println("Hi");
+		try {
+			Thread.sleep(1000); //this will help to call each run() methods with and interval of 1000ms or 1s so to print the output alternately.
+		    //actually Thread.sleep(); pauses the current thread method for a certain interval of time as mentioned
+		    }
+		catch(Exception e)
+		{
+			
+		}
+		}
+	}
+}
+class B extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=10;i++)
+		{
+		System.out.println("Hello");
+		try {
+			Thread.sleep(1000);
+		    }
+		catch(Exception e)
+		{
+			
+		}
+		}
+	}
+}
+public class MultiThread {
+
+	public static void main(String[] args) {
+		A obj1 =new A();
+		B obj2= new B();
+		obj1.start();
+		try {
+			Thread.sleep(10);//this will help to call the main to call each start with an interval of 10ms
+		    }
+		catch(Exception e)
+		{
+			
+		}
+		obj2.start();
+		
+	}
+
+}
