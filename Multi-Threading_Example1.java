f815f969fb370fed4ce312a3093d6af52beaa24b
Aritra Basak <basakaritra10@gmail.com>
2022-07-23 14:08:43 +0530

    Update Multi-Threading_Example1.java

--- Multi-Threading_Example1.java
+++ Multi-Threading_Example1.java
@@ -1,6 +1,6 @@
-/*In the below program we see the use of Multi-threading that we are forcing the CPU to use it's multiple cores.
+/*In the below program we see the use of MultiThreading that we are forcing the CPU to use it's multiple cores.
  * We will make the CPU do each task in each cores.
- * By default JAVA runs a thread at every time we run a program and that thread is called Main Thread.
+ * By default JAVA runs a thread at every time we run a program and that thread is called Main Thread. 
  * Here in the below program we are using extends Thread class to achieve MultiTHreading */
 package placements;
 class A extends Thread //we are extending this class as thread as to make java run each class in each core i.e. running both the classes parallel.
@@ -14,12 +14,12 @@ class A extends Thread //we are extending this class as thread as to make java r
 		{
 		System.out.println("Hi");
 		try {
-			Thread.sleep(1000); //this will help to call each run() methods with and interval of 1000ms or 1s so to print the output alternately.
+		Thread.sleep(1000); //this will help to call each run() methods with and interval of 1000ms or 1s so to print the output alternately.
 		    //actually Thread.sleep(); pauses the current thread method for a certain interval of time as mentioned
 		    }
 		catch(Exception e)
 		{
-			
+		//no need to print anything		
 		}
 		}
 	}
@@ -36,7 +36,7 @@ class B extends Thread
 		    }
 		catch(Exception e)
 		{
-			
+	        //no need to print anything	
 		}
 		}
 	}
@@ -46,16 +46,14 @@ public class MultiThread {
 	public static void main(String[] args) {
 		A obj1 =new A();
 		B obj2= new B();
-		obj1.start();
+		obj1.start();//we can call the start() with object reference of our class because we have inherited all the property of Thread class in ours.
 		try {
 			Thread.sleep(10);//this will help to call the main to call each start with an interval of 10ms
 		    }
 		catch(Exception e)
 		{
-			
+		//no need to print anything	
 		}
 		obj2.start();
-		
 	}
-
 }
