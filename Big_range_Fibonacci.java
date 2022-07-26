afa2e913701ca18176360284ba629b620eafcfa3
Aritra Basak <basakaritra10@gmail.com>
2022-07-26 21:45:50 +0530

    Update Big_range_Fibonacci.java

--- Big_range_Fibonacci.java
+++ Big_range_Fibonacci.java
@@ -3,12 +3,8 @@ import java.util.*;
 public class Solution {
 	static long fibo(long l)
 	{
-		if(l==0)
-			return 0;
-		else if(l==1)
-			return 1;
-		else
-			return (long) (Math.pow(((1+ Math.sqrt(5))/2),l)/ Math.sqrt(5)); //we use this formula instead of the normal because using the normal formula for big range increases the complexity due to recursive tree and pc can't calculate
+		 return (long) ((Math.pow(((1+ Math.sqrt(5))/2),l)-(Math.pow(((1- Math.sqrt(5))/2),l)))/ Math.sqrt(5));
+		//we use this formula instead of the normal because using the normal formula for big range increases the complexity due to recursive tree and pc can't calculate or it will take alot of time
 	}
 
 	public static void main(String[] args) {
