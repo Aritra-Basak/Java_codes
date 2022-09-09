9f0bccbb45b201d242add1f23f1ed1dfe8b28e11
Aritra Basak <basakaritra10@gmail.com>
2022-09-09 21:45:02 +0530

    Create Camel-Casting.java

---
+++ Camel-Casting.java
@@ -0,0 +1,45 @@
+package placements;
+//In Camel Casting we have to print all the Upper-Case Characters in a new line being converted into Lower-Case and print all the Lower-Case Characters in same line converted into Upper Case.
+import java.util.*;
+
+public class Camel {
+
+	static void camel(String a)
+	{
+		System.out.println("In Normal Format:- \n"+a);
+		char [] ch = a.toCharArray();
+		System.out.println("In Camel Casted Format:-");
+		for(char ele:ch)
+		{
+			if(Character.isUpperCase(ele))
+				System.out.print("\n"+Character.toLowerCase(ele));
+			else
+				System.out.print(Character.toUpperCase(ele));
+		}
+		
+				
+	}
+	
+	
+    public static void main(String [] args)
+    {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter the String");
+    	String a = sc.next();// in input for camel casting the string must not have space.
+    	camel(a);
+    	
+    }
+}
+
+
+
+/*
+Enter the String
+HiHello
+In Normal Format:- 
+HiHello
+In Camel Casted Format:- 
+
+hI
+hELLO
+*/
