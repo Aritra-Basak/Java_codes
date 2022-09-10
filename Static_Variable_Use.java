c77adc719374f48c7d629a5d912a8ef36010559c
Aritra Basak <basakaritra10@gmail.com>
2022-09-10 22:41:13 +0530

    Update Static_Variable_Use.java

--- Static_Variable_Use.java
+++ Static_Variable_Use.java
@@ -28,10 +28,12 @@ public class Constant_for_every_instance
     	  Human Aritra = new Human("Aritra Basak", 21,45000,false);
     	  Human Promi = new Human("Promi Paul", 19,45000,false);
     	  Human Hrishin = new Human("Hrishin Dey", 20,35000,false);
-    	  System.out.println(Aritra.name+" - "+Aritra.age+" - "+ Aritra.salary+" - "+Aritra.married+ " The current number of employee w.r.t Aritra is :"+Aritra.employee_number);
-    	  System.out.println(" The current number of employee w.r.t Promi is :"+Promi.employee_number);
-    	  System.out.println(" The current number of employee w.r.t Hrishin is :"+Hrishin.employee_number);
+    	  System.out.println(Aritra.name+" - "+Aritra.age+" - "+ Aritra.salary+" - "+Aritra.married+ " The current number of employee w.r.t Aritra is :"+Human.employee_number);
+    	  System.out.println(" The current number of employee w.r.t Promi is :"+Human.employee_number);
+    	  System.out.println(" The current number of employee w.r.t Hrishin is :"+Human.employee_number);
     	  
     	  //Here the number of employee will be same for all the object reference of any class.
+    	  //so using static variable with its class name will give the same output as with respective object references.
+    	  
     }
 }
