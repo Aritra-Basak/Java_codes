6adf21e8011f91e569c857ed4deb27005d295ad0
Aritra Basak <basakaritra10@gmail.com>
2023-04-20 13:23:10 +0530

    Update Java_Program_to_Find_Memory_Usage_Of_PC.java

--- Java_Program_to_Find_Memory_Usage_Of_PC.java
+++ Java_Program_to_Find_Memory_Usage_Of_PC.java
@@ -18,7 +18,7 @@ public class MemoryUsage {
 		 return (long) ((Math.pow(((1+ Math.sqrt(5))/2),l)-(Math.pow(((1- Math.sqrt(5))/2),l)))/ Math.sqrt(5));
 		//we use this formula instead of the normal because using the normal formula for big range increases the complexity due to recursive tree and pc can't calculate or it will take alot of time
 	}
-
+//Java Program to calculate memory usage while the program is compiling.
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the range's limit");
